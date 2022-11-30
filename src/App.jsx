0bf3dd16648a689bb5eeca65c582fa1388439514
DeveloperAbelCastillo <developerabelcastillo@gmail.com>
2022-11-30 09:28:33 -0600

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -26,20 +26,14 @@ function App() {
     promptInstall.prompt();
   };
 
-  if (!supportsPWA) {
-    return null;
-  }
-
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      {!supportsPWA && <div>No se soporta la isntalacion de PWA</div>}
       <button
         className="link-button"
         id="setup_button"
